d6891c932fd8d318786be3701de1d52178afd2b3
Brett Garber <bgarber0343@gmail.com>
2021-09-17 17:28:38 -0500

    Added launcher and ConnectionUtil

---
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -0,0 +1,30 @@
+package com.revature.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	
+
+	public static Connection getConnection() throws SQLException {
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("problem occurred locating driver");
+		}
+		//These variables holding my DB credentials are hidden in my Environment Variables
+		//Run -> Run Configurations -> Environment -> Then create key/value pairs for these credentials
+		//You may have to stretch out the window to see the "environment" option
+		String url = System.getenv("URL");
+		String username = System.getenv("USERNAME");
+		String password = System.getenv("PASSWORD");
+		
+		return DriverManager.getConnection(url, username, password);
+		
+	}
+	
+}
